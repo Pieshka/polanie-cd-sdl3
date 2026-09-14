3312fba4667fa747e2f0bb44416a6aa4c16d20cc
Pieszka <pszk@pieszka.net>
2026-09-14 15:06:57 +0200

    chore: first commit

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,64 @@
+# --------------------------------------
+# Dependencies
+# --------------------------------------
+
+set(USE_SYSTEM_SDL3 OFF)
+set(USE_SYSTEM_SDL3_MIXER OFF)
+
+# --------------------------------------
+# Try system libraries on Linux
+# --------------------------------------
+
+if(UNIX)
+
+    find_package(SDL3 CONFIG QUIET)
+    if(SDL3_FOUND)
+        message(STATUS "Using system SDL3")
+        set(USE_SYSTEM_SDL3 ON)
+    endif()
+	
+	find_package(SDL3_mixer CONFIG QUIET)
+    if(SDL3_MIXER_FOUND)
+        message(STATUS "Using system SDL3_Mixer")
+        set(USE_SYSTEM_SDL3_MIXER ON)
+    endif()
+
+endif()
+
+
+# --------------------------------------
+# Fetch missing libraries
+# --------------------------------------
+
+# SDL3
+if(NOT USE_SYSTEM_SDL3)
+
+    add_subdirectory(3rdparty/SDL EXCLUDE_FROM_ALL)
+
+endif()
+
+if(NOT USE_SYSTEM_SDL3_MIXER)
+
+    add_subdirectory(3rdparty/SDL_mixer EXCLUDE_FROM_ALL)
+
+endif()
+
+# --------------------------------------
+# Install runtime dependencies
+# --------------------------------------
+
+if(TARGET SDL3-shared AND NOT WIN32)
+    install(
+        TARGETS SDL3-shared
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    )
+endif()
+
+if(TARGET SDL3_mixer-shared AND NOT WIN32)
+    install(
+        TARGETS SDL3_mixer-shared
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    )
+endif()
